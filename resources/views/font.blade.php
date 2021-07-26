ac94947556bf2dc17fbbdf8f7b5bfa7789325a5b
Koramit Pichanaharee <koramit.p@gmail.com>
2021-07-26 18:37:01 +0700

    host fonts

---
+++ resources/views/font.blade.php
@@ -0,0 +1,218 @@
+<style>
+    /* thai */
+@font-face {
+  font-family: 'Krub';
+  font-style: italic;
+  font-weight: 300;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlGdRyC6CRYbkQipLNCUIASW_zqgER5.woff2') }}) format('woff2');
+  unicode-range: U+0E01-0E5B, U+200C-200D, U+25CC;
+}
+/* vietnamese */
+/* @font-face {
+  font-family: 'Krub';
+  font-style: italic;
+  font-weight: 300;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlGdRyC6CRYbkQipLNCS4ASW_zqgER5.woff2') }}) format('woff2');
+  unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
+} */
+/* latin-ext */
+@font-face {
+  font-family: 'Krub';
+  font-style: italic;
+  font-weight: 300;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlGdRyC6CRYbkQipLNCSoASW_zqgER5.woff2') }}) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Krub';
+  font-style: italic;
+  font-weight: 300;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlGdRyC6CRYbkQipLNCRIASW_zqgA.woff2') }}) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+/* thai */
+@font-face {
+  font-family: 'Krub';
+  font-style: italic;
+  font-weight: 600;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlGdRyC6CRYbkQi0LVCUIASW_zqgER5.woff2') }}) format('woff2');
+  unicode-range: U+0E01-0E5B, U+200C-200D, U+25CC;
+}
+/* vietnamese */
+/* @font-face {
+  font-family: 'Krub';
+  font-style: italic;
+  font-weight: 600;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlGdRyC6CRYbkQi0LVCS4ASW_zqgER5.woff2') }}) format('woff2');
+  unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
+} */
+/* latin-ext */
+@font-face {
+  font-family: 'Krub';
+  font-style: italic;
+  font-weight: 600;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlGdRyC6CRYbkQi0LVCSoASW_zqgER5.woff2') }}) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Krub';
+  font-style: italic;
+  font-weight: 600;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlGdRyC6CRYbkQi0LVCRIASW_zqgA.woff2') }}) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+/* thai */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZuo4GKJyRooVef7agQ.woff2') }}) format('woff2');
+  unicode-range: U+0E01-0E5B, U+200C-200D, U+25CC;
+}
+/* vietnamese */
+/* @font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZuo4GLlyRooVef7agQ.woff2') }}) format('woff2');
+  unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
+} */
+/* latin-ext */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZuo4GLhyRooVef7agQ.woff2') }}) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZuo4GLZyRooVef4.woff2') }}) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+/* thai */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlLdRyC6CRYblUaDZtQS6AvcA.woff2') }}) format('woff2');
+  unicode-range: U+0E01-0E5B, U+200C-200D, U+25CC;
+}
+/* vietnamese */
+/* @font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlLdRyC6CRYbk4aDZtQS6AvcA.woff2') }}) format('woff2');
+  unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
+} */
+/* latin-ext */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlLdRyC6CRYbk8aDZtQS6AvcA.woff2') }}) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlLdRyC6CRYbkEaDZtQS6A.woff2') }}) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+/* thai */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZrI5GKJyRooVef7agQ.woff2') }}) format('woff2');
+  unicode-range: U+0E01-0E5B, U+200C-200D, U+25CC;
+}
+/* vietnamese */
+/* @font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZrI5GLlyRooVef7agQ.woff2') }}) format('woff2');
+  unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
+} */
+/* latin-ext */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZrI5GLhyRooVef7agQ.woff2') }}) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZrI5GLZyRooVef4.woff2') }}) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+/* thai */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZp4-GKJyRooVef7agQ.woff2') }}) format('woff2');
+  unicode-range: U+0E01-0E5B, U+200C-200D, U+25CC;
+}
+/* vietnamese */
+/* @font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZp4-GLlyRooVef7agQ.woff2') }}) format('woff2');
+  unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
+} */
+/* latin-ext */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZp4-GLhyRooVef7agQ.woff2') }}) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Krub';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: url({{ asset('fonts/krub/v4/sZlEdRyC6CRYZp4-GLZyRooVef4.woff2') }}) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+</style>
