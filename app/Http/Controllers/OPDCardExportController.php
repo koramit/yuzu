14384cc6ba8d2e2635bdc798a1a1df0967d25ee7
Koramit Pichanaharee <koramit.p@gmail.com>
2021-07-28 08:45:44 +0700

    add visit excel

--- app/Http/Controllers/OPDCardExportController.php
+++ app/Http/Controllers/OPDCardExportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Visit;
+use Illuminate\Support\Carbon;
 use Rap2hpoutre\FastExcel\Facades\FastExcel;
 
 class OPDCardExportController extends Controller
@@ -15,9 +16,25 @@ class OPDCardExportController extends Controller
                        ->transform(function ($visit) {
                            return [
                                 'id' => $visit->id,
+                                'date_visit' => $visit->date_visit->format('d-M-Y'),
                                 'HN' => $visit->hn,
                                 'name' => $visit->patient_name,
-                                'result' => $visit->form['result'] ?? null,
+                                'patient_type' => $visit->patient_type,
+                                'screen_type' => $visit->screen_type,
+                                'symptoms' => $visit->form['symptoms']['asymptomatic_symptom'] ? 'ไม่มีอาการ' : 'มีอาการ',
+                                'onset' => $visit->form['symptoms']['date_symptom_start'] ? Carbon::create($visit->form['symptoms']['date_symptom_start'])->format('d-M-Y') : null,
+                                'fever' => $visit->form['symptoms']['fever'] ? 'YES' : 'NO',
+                                'cough' => $visit->form['symptoms']['cough'] ? 'YES' : 'NO',
+                                'sore_throat' => $visit->form['symptoms']['sore_throat'] ? 'YES' : 'NO',
+                                'rhinorrhoea' => $visit->form['symptoms']['rhinorrhoea'] ? 'YES' : 'NO',
+                                'sputum' => $visit->form['symptoms']['sputum'] ? 'YES' : 'NO',
+                                'fatigue' => $visit->form['symptoms']['fatigue'] ? 'YES' : 'NO',
+                                'anosmia' => $visit->form['symptoms']['anosmia'] ? 'YES' : 'NO',
+                                'loss_of_taste' => $visit->form['symptoms']['loss_of_taste'] ? 'YES' : 'NO',
+                                'diarrhea' => $visit->form['symptoms']['diarrhea'] ? 'YES' : 'NO',
+                                'other_symptoms' => $visit->form['symptoms']['other_symptoms'] ? 'YES' : 'NO',
+                                'np_swab' => $visit->form['management']['np_swab'] ? 'YES' : 'NO',
+                                'np_swab_result' => $visit->form['management']['np_swab'] ? ($visit->form['result'] ?? 'pending') : null,
                             ];
                        });
 
--- app/Http/Controllers/VisitExportController.php
+++ app/Http/Controllers/VisitExportController.php
@@ -14,10 +14,16 @@ class VisitExportController extends Controller
                        ->get()
                        ->transform(function ($visit) {
                            return [
-                                'id' => $visit->id,
+                                'วันที่ตรวจ' => $visit->date_visit->format('d-M-Y'),
                                 'HN' => $visit->hn,
-                                'name' => $visit->patient_name,
-                                'result' => $visit->form['result'] ?? null,
+                                'ชื่อผู้ป่วย' => $visit->patient_name,
+                                'ประเภทผู้ป่วย' => $visit->patient_type,
+                                'ประเภทการตรวจ' => $visit->screen_type,
+                                'สิทธิ์' => $visit->form['patient']['insurance'],
+                                'กลุ่ม' => ($visit->patient_type === 'บุคคลทั่วไป' && $visit->screen_type === 'เริ่มตรวจใหม่') ? 'walk-in' : 'นัด-staff',
+                                'เริ่มคัดกรอง' => $visit->enlisted_screen_at ? $visit->enlisted_screen_at->tz('asia/bangkok')->format('H:i') : null,
+                                'จำหน่าย' => $visit->discharged_at ? $visit->discharged_at->tz('asia/bangkok')->format('H:i') : null,
+                                'ใช้เวลา' => ($visit->enlisted_screen_at && $visit->discharged_at) ? $visit->discharged_at->diffInMinutes($visit->enlisted_screen_at) : null,
                             ];
                        });
 
