69c0eff6589a820278c108956d204a397fa2deee
Koramit Pichanaharee <koramit.p@gmail.com>
2021-08-23 19:15:15 +0700

    add change home page

---
+++ app/Http/Controllers/PreferencesController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+use Inertia\Inertia;
+
+class PreferencesController extends Controller
+{
+    protected $pages = [
+        'visits.screen-list' => 'ห้องคัดกรอง',
+        'visits.exam-list' => 'ห้องตรวจ',
+        'visits.enqueue-swab-list' => 'จัดกระติก',
+        'visits.swab-list' => 'ห้อง Swab',
+        'visits.mr-list' => 'เวชระเบียน',
+        'visits.queue-list' => 'ธุรการ',
+        'visits.today-list' => 'รายการเคสวันนี้',
+        'visits.lab-list' => 'ผลแลปวันนี้',
+        'visits' => 'รายการเคสทั้งหมด',
+    ];
+
+    public function show()
+    {
+        Request::session()->flash('page-title', 'ตั้งค่า');
+
+        $user = Auth::user();
+
+        return Inertia::render('Auth/Preferences', [
+            'selectHomePage' => [
+                'currentHomePage' => $this->pages[$user->home_page],
+                'pages' => [
+                    ['label' => 'ห้องคัดกรอง', 'can' => $user->can('view_screen_list')],
+                    ['label' => 'ห้องตรวจ', 'can' => $user->can('view_exam_list')],
+                    ['label' => 'จัดกระติก', 'can' => $user->can('view_enqueue_swab_list')],
+                    ['label' => 'ห้อง Swab', 'can' => $user->can('view_swab_list')],
+                    ['label' => 'เวชระเบียน', 'can' => $user->can('view_mr_list')],
+                    ['label' => 'ธุรการ', 'can' => $user->can('view_queue_list')],
+                    ['label' => 'รายการเคสวันนี้', 'can' => $user->can('view_today_list')],
+                    ['label' => 'ผลแลปวันนี้', 'can' => $user->can('view_any_visits')],
+                    ['label' => 'รายการเคสทั้งหมด', 'can' => $user->can('view_any_visits')],
+                ],
+            ],
+        ]);
+    }
+
+    public function update()
+    {
+        $data = Request::all();
+        if (isset($data['home_page'])) {
+            return $this->updateHomePage($data);
+        } else {
+            return ['ok' => true];
+        }
+    }
+
+    protected function updateHomePage($data)
+    {
+        $newHomePage = null;
+        foreach ($this->pages as $route => $name) {
+            if ($data['home_page'] === $name) {
+                $newHomePage = $route;
+                break;
+            }
+        }
+
+        if ($newHomePage) {
+            $user = Auth::user();
+            $user->home_page = $newHomePage;
+            $user->save();
+        }
+
+        return ['ok' => true];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImportAppointmentsController;
 use App\Http\Controllers\OPDCardExportController;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PreferencesController;
 use App\Http\Controllers\PrintOPDCardController;
 use App\Http\Controllers\ResourceEmployeesController;
 use App\Http\Controllers\ResourcePatientsController;
@@ -55,9 +56,12 @@ Route::get('terms-and-policies', [PagesController::class, 'terms'])
      ->name('terms');
 
 // preferences
-Route::get('/', HomeController::class)
+Route::get('/', [PreferencesController::class, 'show'])
      ->middleware('auth')
      ->name('preferences');
+Route::patch('/', [PreferencesController::class, 'update'])
+     ->middleware('auth')
+     ->name('preferences.update');
 
 // home
 Route::get('home', HomeController::class)
