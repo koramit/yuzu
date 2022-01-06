d54cb8c3496ced3f792b1f32f95f52af6df3cecf
Koramit Pichanaharee <koramit.p@gmail.com>
2022-01-06 20:26:37 +0700

    remove token

--- app/Http/Controllers/Services/LINEWebhooksController.php
+++ app/Http/Controllers/Services/LINEWebhooksController.php
@@ -7,12 +7,12 @@ use Illuminate\Support\Facades\Request;
 
 class LINEWebhooksController extends Controller
 {
-    public function __invoke($token)
+    public function __invoke()
     {
         $hash = hash_hmac('sha256', Request::getContent(), config('services.line.bot_secret'), true);
         $signature = base64_encode($hash);
 
-        if ($signature !== Request::header('x-line-signature')) {
+        if (Request::header('x-line-signature') !== $signature) {
             abort(404);
         }
 
