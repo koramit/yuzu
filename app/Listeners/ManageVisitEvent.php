01afe6da97750179a1b7e67b4d9d5868ce54622a
Koramit Pichanaharee <koramit.p@gmail.com>
2022-01-11 23:15:38 +0700

    exclude lab report

--- app/Listeners/ManageVisitEvent.php
+++ app/Listeners/ManageVisitEvent.php
@@ -37,7 +37,10 @@ class ManageVisitEvent
             Cache::put('screen-list-new', time());
         }
 
-        if ($event->visit->status === 'discharged' && config('app.env') === 'production') {
+        if (
+            $event->visit->status === 'discharged' &&
+            !($event->visit->form['management']['np_swab_result'] ?? null) && // exclude on lab reported
+            config('app.env') === 'production') {
             (new SiITManager)->manage($event->visit);
         }
     }
