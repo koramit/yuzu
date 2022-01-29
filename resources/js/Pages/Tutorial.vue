e329875301e595a3b2de782f5428f5d06e5e6b74
Koramit Pichanaharee <koramit.p@gmail.com>
2022-01-29 19:00:17 +0700

    tweak

--- resources/js/Pages/Tutorial.vue
+++ resources/js/Pages/Tutorial.vue
@@ -11,15 +11,16 @@
             <ul class="list-outside list-decimal pl-4 mt-6">
                 <li class="my-6">
                     Login ระบบ ARI คลินิก เพื่อตั้งค่าการแจ้งเตือนได้ที่ <a
-                        href="https://simed.mahidol.ac.th/yuzu/login"
+                        :href="route('login')"
                         target="_blank"
                         class="text-blue-400"
-                    >https://simed.mahidol.ac.th/yuzu/login</a> ด้วย Siriraj AD (รหัสเข้า eDoc/SiIT)
+                        v-text="route('login')"
+                    />  ด้วย Siriraj AD (รหัสเข้า eDoc/SiIT)
                 </li>
                 <li class="my-6">
                     เมื่อ Login แล้วจะอยู่ที่หน้า <span class="font-semibold">"ตั้งค่า"</span> หากพบเป็นหน้าอื่นสามารถไปยังหน้าตั้งค่าได้จากเมนูมุมบนขวา
                     <img
-                        class="mx-auto w-80 rounded shadow"
+                        class="mx-auto lg:ml-12 w-80 rounded shadow"
                         :src="route('home')+'/image/tutorial/personal-menu.png'"
                         alt=""
                     >
@@ -27,7 +28,7 @@
                 <li class="my-6">
                     ทำการตั้งค่าการแจ้งเตือนผ่านทาง LINE ตามคำแนะนำในระบบ
                     <img
-                        class="mx-auto w-80 rounded shadow"
+                        class="mx-auto lg:ml-12 w-80 rounded shadow"
                         :src="route('home')+'/image/tutorial/link-line.png'"
                         alt=""
                     >
@@ -35,7 +36,7 @@
                 <li class="my-6">
                     ทำการยืนยัน HN ในหน้าการตั้งค่า
                     <img
-                        class="mx-auto w-80 rounded shadow"
+                        class="mx-auto lg:ml-12 w-80 rounded shadow"
                         :src="route('home')+'/image/tutorial/link-patient.png'"
                         alt=""
                     >
@@ -43,7 +44,7 @@
                 <li class="my-6">
                     เมื่อทำการตั้งค่าการแจ้งเตือนและยืนยัน HN สำเร็จเรียบร้อยแล้ว สามารถสอบถามลำดับคิวทำสวอปของท่าน โดยการพิมพ์คำว่า <span class="font-semibold">"คิวตรวจ"</span> ส่งมาใน LINE
                     <img
-                        class="mx-auto w-80 rounded shadow"
+                        class="mx-auto lg:ml-12 w-80 rounded shadow"
                         :src="route('home')+'/image/tutorial/get-swab-queue.png'"
                         alt=""
                     >
@@ -51,7 +52,7 @@
                 <li class="my-6">
                     เมื่อใกล้ถึงคิวของท่านจะมีการแจ้งเตือนผ่านทาง LINE
                     <img
-                        class="mx-auto w-80 rounded shadow"
+                        class="mx-auto lg:ml-12 w-80 rounded shadow"
                         :src="route('home')+'/image/tutorial/calling-queue-notify.png'"
                         alt=""
                     >
