d42b8e2610a3abe2ae50ba61c4b98484d93b91de
Koramit Pichanaharee <koramit.p@gmail.com>
2022-02-15 20:03:25 +0700

    exclude csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
+        'koto',
         'ww',
-        'webhook/line'
+        'webhook/line',
     ];
 }
