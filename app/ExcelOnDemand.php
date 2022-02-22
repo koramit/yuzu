7080f15b59bd425293ac526a201790f3f86ad7e5
Koramit Pichanaharee <koramit.p@gmail.com>
2022-02-22 21:26:57 +0700

    export on demand

---
+++ app/ExcelOnDemand.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App;
+
+use App\Models\Visit;
+use App\Traits\OPDCardExportable;
+use Rap2hpoutre\FastExcel\Facades\FastExcel;
+
+class ExcelOnDemand
+{
+    use OPDCardExportable;
+
+    public function export()
+    {
+        $data = $this->kantarida2202();
+
+        return FastExcel::data(collect($data))->export(storage_path('app/temp/kk.xlsx'));
+    }
+
+    protected function kantarida2202()
+    {
+        $visits = Visit::with('patient')
+                        ->where('date_visit', '<', '2022-01-01')
+                        ->where('patient_type', 2) // จนท
+                        ->where('swabbed', true)
+                        ->where('status', 4)
+                        ->get();
+
+        return $visits->transform(function ($v) {
+            $visit = $this->allData($v);
+
+            return [
+                'name' => $visit['name'],
+                'HN' => $visit['HN'],
+                'gender' => $visit['gender'],
+                'age' => $visit['age'],
+                'date_of_visit' => $visit['date_visit'],
+                'sap_id' => $visit['sap_id'],
+                'position' => $visit['position'],
+                'division' => $visit['division'],
+                'service_location' => $visit['service_location'],
+                'menstruation' => $visit['menstruation'],
+                'latest_date_of_exposure' => $visit['date_latest_expose'],
+                'temperature_celsius' => $visit['temperature_celsius'],
+                // date 0f swab
+                'symptoms' => $visit['symptoms'],
+                'fever' => $visit['fever'],
+                'cough' => $visit['cough'],
+                'sore_throat' => $visit['sore_throat'],
+                'rhinorrhoea' => $visit['rhinorrhoea'],
+                'sputum' => $visit['sputum'],
+                'fatigue' => $visit['fatigue'],
+                'anosmia' => $visit['anosmia'],
+                'loss_of_taste' => $visit['loss_of_taste'],
+                'myalgia' => $visit['myalgia'],
+                'diarrhea' => $visit['diarrhea'],
+                'other_symptoms' => $visit['other_symptoms'],
+                'onset_of_symtom' => $visit['onset'],
+
+                'exposure_evaluation' => $visit['exposure_evaluation'],
+                // 'date_latest_expose' => $visit['date_latest_expose'],
+                'contact' => $visit['contact'],
+                'contact_type' => $visit['contact_type'],
+                'contact_detail' => $visit['contact_detail'],
+                'hot_spot' => $visit['hot_spot'],
+                'hot_spot_detail' => $visit['hot_spot_detail'],
+                'other_detail' => $visit['other_detail'],
+
+                'no_comorbids' => $visit['no_comorbids'],
+                'dm' => $visit['dm'],
+                'ht' => $visit['ht'],
+                'dlp' => $visit['dlp'],
+                'obesity' => $visit['obesity'],
+
+                'weight' => $visit['weight'],
+
+                'unvaccinated' => $visit['unvaccinated'],
+                'Sinovac' => $visit['Sinovac'],
+                'Sinopharm' => $visit['Sinopharm'],
+                'AstraZeneca' => $visit['AstraZeneca'],
+                'Moderna' => $visit['Moderna'],
+                'Pfizer' => $visit['Pfizer'],
+                'number_of_vaccination_dose' => $visit['doses'],
+                'date_of_latest_vaccination' => $visit['date_latest_vacciniated'],
+
+                'np_swab_result' => $visit['np_swab_result'],
+                'ct_value' => $visit['np_swab_result_note'],
+            ];
+        });
+    }
+}
---
+++ app/Traits/OPDCardExportable.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Visit;
+use Carbon\Carbon;
+
+trait OPDCardExportable
+{
+    protected function allData(Visit $visit)
+    {
+        $form = $visit->form;
+        $profile = $visit->patient->profile;
+
+        return [
+            'date_visit' => $visit->date_visit->format('d-M-Y'),
+            'HN' => $visit->hn,
+            'name' => $visit->patient_name,
+            'patient_type' => $visit->patient_type,
+            'screen_type' => $visit->screen_type,
+            'gender' => $visit->patient->gender,
+            'menstruation' => $visit->menstruation,
+            'age' => $visit->age_at_visit,
+            'age_unit' => $visit->age_at_visit_unit,
+            'department' => $visit->patient_department,
+
+            'insurance' => $form['patient']['insurance'],
+            'tel_no' => $form['patient']['tel_no'],
+            'tel_no_alt' => $form['patient']['tel_no_alt'],
+            'address' => $profile['address'],
+            'subdistrict' => $profile['subdistrict'],
+            'district' => $profile['district'],
+            'postcode' => $profile['postcode'],
+            'province' => $profile['province'],
+
+            'sap_id' => $form['patient']['sap_id'],
+            'position' => $form['patient']['position'],
+            'division' => $form['patient']['division'],
+            'service_location' => $form['patient']['service_location'] ?? null,
+            'risk' => $form['patient']['risk'],
+            'date_latest_expose_by_im' => $this->castDate($form['patient']['date_latest_expose_by_im']),
+
+            'temperature_celsius' => $form['patient']['temperature_celsius'],
+            'o2_sat' => $form['patient']['o2_sat'],
+            'date_swabbed' => $this->castDate($form['patient']['date_swabbed']),
+            'date_reswabbed' => $this->castDate($form['patient']['date_reswabbed']),
+            'passport_no' => $form['patient']['passport_no'] ?? null,
+            'national_id' => $visit->patient->profile['document_id'],
+
+            'symptoms' => $form['symptoms']['asymptomatic_symptom'] ? 'ไม่มีอาการ' : 'มีอาการ',
+            'onset' => $form['symptoms']['date_symptom_start'] ? Carbon::create($form['symptoms']['date_symptom_start'])->format('d-M-Y') : null,
+            'fever' => $form['symptoms']['fever'] ? 'YES' : 'NO',
+            'cough' => $form['symptoms']['cough'] ? 'YES' : 'NO',
+            'sore_throat' => $form['symptoms']['sore_throat'] ? 'YES' : 'NO',
+            'rhinorrhoea' => $form['symptoms']['rhinorrhoea'] ? 'YES' : 'NO',
+            'sputum' => $form['symptoms']['sputum'] ? 'YES' : 'NO',
+            'fatigue' => $form['symptoms']['fatigue'] ? 'YES' : 'NO',
+            'anosmia' => $form['symptoms']['anosmia'] ? 'YES' : 'NO',
+            'loss_of_taste' => $form['symptoms']['loss_of_taste'] ? 'YES' : 'NO',
+            'myalgia' => $form['symptoms']['myalgia'] ? 'YES' : 'NO',
+            'diarrhea' => $form['symptoms']['diarrhea'] ? 'YES' : 'NO',
+            'other_symptoms' => $form['symptoms']['other_symptoms'] ? 'YES' : 'NO',
+
+            'exposure_evaluation' =>  $form['exposure']['evaluation'],
+            'date_latest_expose' => $this->castDate($form['exposure']['date_latest_expose']),
+            'contact' =>  $form['exposure']['contact'] ? 'YES' : 'NO',
+            'contact_type' =>  $form['exposure']['contact_type'],
+            'contact_detail' =>  $form['exposure']['contact_detail'] ? str_replace("\n", ' ', $form['exposure']['contact_detail']) : null,
+            'hot_spot' =>  $form['exposure']['hot_spot'] ? 'YES' : 'NO',
+            'hot_spot_detail' =>  $form['exposure']['hot_spot_detail'] ? str_replace("\n", ' ', $form['exposure']['hot_spot_detail']) : null,
+            'other_detail' =>  $form['exposure']['other_detail'] ? str_replace("\n", ' ', $form['exposure']['other_detail']) : null,
+
+            'no_comorbids' => $form['comorbids']['no_comorbids'] ? 'YES' : 'NO',
+            'dm' => $form['comorbids']['dm'] ? 'YES' : 'NO',
+            'ht' => $form['comorbids']['ht'] ? 'YES' : 'NO',
+            'dlp' => $form['comorbids']['dlp'] ? 'YES' : 'NO',
+            'obesity' => $form['comorbids']['obesity'] ? 'YES' : 'NO',
+            'weight' => $visit->weight,
+            'height' => $form['patient']['height'],
+            'BMI' => $form['patient']['weight'] && $form['patient']['height']
+                ? ($form['patient']['weight'] / $form['patient']['height'] / $form['patient']['height'] * 10000)
+                : null,
+            'other_comorbids' => $form['comorbids']['other_comorbids'],
+
+            'unvaccinated' => $form['vaccination']['unvaccinated'] ? 'YES' : 'NO',
+            'Sinovac' => $form['vaccination']['Sinovac'] ? 'YES' : 'NO',
+            'Sinopharm' => $form['vaccination']['Sinopharm'] ? 'YES' : 'NO',
+            'AstraZeneca' => $form['vaccination']['AstraZeneca'] ? 'YES' : 'NO',
+            'Moderna' => $form['vaccination']['Moderna'] ? 'YES' : 'NO',
+            'Pfizer' => $form['vaccination']['Pfizer'] ? 'YES' : 'NO',
+            'doses' => $form['vaccination']['doses'],
+            'date_latest_vacciniated' => $this->castDate($form['vaccination']['date_latest_vacciniated']),
+
+            'no_symptom' => $form['diagnosis']['no_symptom'] ? 'YES' : 'NO',
+            'suspected_covid_19' => $form['diagnosis']['suspected_covid_19'] ? 'YES' : 'NO',
+            'uri' => $form['diagnosis']['uri'] ? 'YES' : 'NO',
+            'suspected_pneumonia' => $form['diagnosis']['suspected_pneumonia'] ? 'YES' : 'NO',
+            'other_diagnosis' => $form['diagnosis']['other_diagnosis'] ? 'YES' : 'NO',
+
+            'np_swab' => ($form['management']['np_swab'] && ($visit->swabbed ?? false)) ? 'YES' : 'NO',
+            'tube_no' => $form['management']['specimen_no'],
+            'np_swab_result' => $form['management']['np_swab_result'],
+            'np_swab_result_note' => $form['management']['np_swab_result_note'],
+            'other_tests' => $form['management']['other_tests'] ? str_replace("\n", ' ', $form['management']['other_tests']) : null,
+            'home_medication' => $form['management']['home_medication'] ? str_replace("\n", ' ', $form['management']['home_medication']) : null,
+
+            'recommendation' => $this->recommendation($form['recommendation']['choice']),
+            'date_isolation_end' => $this->castDate($form['recommendation']['date_isolation_end']),
+            'date_reswab' => $this->castDate($form['recommendation']['date_reswab']),
+            'date_reswab_next' => $this->castDate($form['recommendation']['date_reswab_next']),
+
+            'note' => $form['note'] ? str_replace("\n", ' ', $form['note']) : null,
+        ];
+    }
+
+    protected function castDate($dateStr)
+    {
+        if (! $dateStr) {
+            return null;
+        }
+
+        return Carbon::create($dateStr)->format('d M Y');
+    }
+
+    protected function recommendation($value)
+    {
+        if (! $value) {
+            return null;
+        }
+
+        return [
+            11  => 'ไปทำงานได้โดยใส่หน้ากากอนามัยตลอดเวลา',
+            12  => 'ลางาน 1 - 2 วัน หากอาการดีขึ้น ไปทำงานได้โดยใส่หน้ากากอนามัยตลอดเวลา',
+            13 => 'ลางาน กักตัวเองที่บ้าน ห้ามพบปะผู้อื่นจนครบ 14 วัน',
+        ][$value];
+    }
+}
