5f45a1902632b57f08a8533f3cf6e4254413d80b
Koramit Pichanaharee <koramit.p@gmail.com>
2022-05-18 06:22:54 +0700

    reschedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,12 +38,12 @@ class Kernel extends ConsoleKernel
         $schedule->call(fn () => DutyToken::generate())->weeklyOn(7, '20:14');
 
         // drink water
-        $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('23:06'); // 06:06
-        $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('02:09'); // 09:09
-        $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('04:11'); // 11:11
-        $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('07:14'); // 14:14
-        $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('10:17'); // 17:17
-        $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('13:20'); // 20:20
+        // $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('23:06'); // 06:06
+        // $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('02:09'); // 09:09
+        // $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('04:11'); // 11:11
+        // $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('07:14'); // 14:14
+        // $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('10:17'); // 17:17
+        // $schedule->call(fn () => DrinkWaterNotification::run())->dailyAt('13:20'); // 20:20
 
         // clear patients
         $schedule->call(fn () => ClearPatientNotification::run())->dailyAt('08:40'); // 15:40
@@ -51,16 +51,16 @@ class Kernel extends ConsoleKernel
         $schedule->call(fn () => ClearPatientNotification::run())->dailyAt('09:30'); // 16:30
 
         // croissant need help
-        $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('12:20'); // 19:20
-        $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('12:50'); // 19:50
-        $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('13:20'); // 20:20
-        $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('13:50'); // 20:50
-        $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('14:20'); // 21:20
-        $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('14:50'); // 21:50
-        $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('15:20'); // 22:20
-        $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('15:50'); // 22:50
-        $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('16:20'); // 23:20
-        $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('16:50'); // 23:50
+        // $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('12:20'); // 19:20
+        // $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('12:50'); // 19:50
+        // $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('13:20'); // 20:20
+        // $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('13:50'); // 20:50
+        // $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('14:20'); // 21:20
+        // $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('14:50'); // 21:50
+        // $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('15:20'); // 22:20
+        // $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('15:50'); // 22:50
+        // $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('16:20'); // 23:20
+        // $schedule->call(fn () => CroissantNeedHelpNotification::run())->dailyAt('16:50'); // 23:50
 
         // update patient name after clinic closed for certification
         $schedule->call(fn () => UpdatePatientName::run())->dailyAt('12:00'); // 19:00
